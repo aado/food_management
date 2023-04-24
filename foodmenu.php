86fb125b7c6192ab8c28f0309efc5fad7a88d568
Jezza <jezzaedano98@gmail.com>
2023-04-24 16:47:22 +0800

    cart

--- foodmenu.php
+++ foodmenu.php
@@ -129,52 +129,8 @@ body {font-family: "Lato", sans-serif;}
 </div>
 <?php } ?>
 </div>
-<div class="col" style="margin-left:30px;">
+<div class="col" style="margin-right:1%;">
   <h4><u> ORDER DETAILS </u></h4>
-
-  <?php
-    if(!empty($_SESSION["cart"])){
-    ?>      
-      <h3>Your Cart</h3>    
-      <table class="table table-striped table-bordered">
-       <thead class="thead-dark">
-      <tr>
-      <th width="40%">Food Name</th>
-      <th width="10%">Quantity</th>
-      <th width="20%">Price Details</th>
-      <th width="15%">Order Total</th>
-      <th width="5%">Action</th>
-      </tr>
-      </thead>
-      <?php
-      $total = 0;
-      foreach($_SESSION["cart"] as $keys => $values){
-      ?>
-        <tr>
-        <td><?php echo $values["item_name"]; ?></td>
-        <td class="text-center"><?php echo $values["item_quantity"] ?></td>
-        <td class="text-end">$ <?php echo $values["item_price"]; ?></td>
-        <td class="text-end">$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>
-        <td><a href="cart.php?action=delete&id=<?php echo $values["food_id"]; ?>" onclick="if(confirm('Are you sure to remove this item from cart list?') === false) { event.preventDefault() }"><span class="text-danger text-decoration-none">Remove</span></a></td>
-        </tr>
-        <?php 
-        $total = $total + ($values["item_quantity"] * $values["item_price"]);
-      }
-      ?>
-      <tr>
-      <td colspan="3" class="text-end">Total</td>
-      <td class="text-end">$ <?php echo number_format($total, 2); ?></td>
-      <td></td>
-      </tr>
-      </table>
-      <div class="text-end">
-        <a href="cart.php?action=empty"><button class="rounded-0 btn btn-danger"><span class="fa fa-trash"></span> Empty Cart</button></a>
-        <a href="index.php"><button class="rounded-0 btn btn-warning">Add more items</button></a>
-        <a href="checkout.php"><button class="rounded-0 btn btn-success pull-right"><span class="fa fa-share-alt"></span> Check Out</button></a>
-      </div>
-    <?php
-    } elseif(empty($_SESSION["cart"])){
-    ?>
       <div class="container">
         <!-- <div class="jumbotron py-5 my-5"> -->
         <!-- <h3 class='text-center'> No orders yet?      -->
@@ -200,10 +156,9 @@ body {font-family: "Lato", sans-serif;}
       </tr>
       </tfoot>
     </table>
-    <?php
-    }
-    ?>  
-
+    <div style="">
+    <button type="button" style="margin-left:40%;" class="btn btn-warning btn-lg"> Checkout </button>
+    </div>
  </div>   
 </div>
 </div>
